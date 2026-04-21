9619395caffdbaa7d3c1016130f2d2d10de112a5
taitbt3124 <taitbt3124@gmail.com>
2026-04-21 19:35:00 +0700

    Kha1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        B1: Khai báo đối tượng scanner
+        Scanner scanner = new Scanner(System.in);
+//        B2: Thông báo nhập
+        System.out.print("Nhập bán kính case 1: ");
+        float radios_1 = scanner.nextFloat();
+        float acreage = (radios_1 * radios_1) * 3.14f ;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.print("Nhập bán kính case 2: ");
+        float radios_2 = scanner.nextFloat();
+        float acreage2 = (radios_2 * radios_1) * 3.14f ;
+
+        System.out.print("Nhập bán kính case 3: ");
+        float radios_3 = scanner.nextFloat();
+        float acreage3 = (radios_3 * radios_3) * 3.14f ;
+
+        System.out.print("Nhập bán kính case 4: ");
+        float radios_4 = scanner.nextFloat();
+        float acreage4 = (radios_4 * radios_4) * 3.14f ;
+
+        System.out.print("Nhập bán kính case 5: ");
+        float radios_5 = scanner.nextFloat();
+        float acreage5 = (radios_5 * radios_5) * 3.14f ;
+
+
+
+        System.out.printf(" Diện tích là: %.2f", acreage);
+        System.out.printf("%n Diện tích là: %.2f", acreage2);
+        System.out.printf("%n Diện tích là: %.2f", acreage3);
+        System.out.printf("%n Diện tích là: %.2f", acreage4);
+        System.out.printf("%n Diện tích là: %.2f", acreage5);
+
+
+
+
+
     }
 }
--- src/module_1/bai1_kha.java
+++ src/module_1/bai1_kha.java
@@ -1,4 +1,36 @@
 package module_1;
 
+import java.util.Scanner;
+
 public class bai1_kha {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("R1: ");
+        float r1 = sc.nextInt();
+        float result1 = (r1 * r1) * 3.14f;
+
+        System.out.print("R2: ");
+        float r2 = sc.nextInt();
+        float result2 = (r2 * r2) * 3.14f;
+
+        System.out.print("R3: ");
+        float r3 = sc.nextInt();
+        float result3 = (r3 * r3) * 3.14f;
+
+        System.out.print("R4: ");
+        float r4 = sc.nextInt();
+        float result4 = (r4 * r4) * 3.14f;
+
+        System.out.print("R5: ");
+        float r5 = sc.nextInt();
+        float result5 = (r5 * r5) * 3.14f;
+
+
+        System.out.printf("%n Bán kính với r1 (%.2f) là %.2f",r1,result1);
+        System.out.printf("%n Bán kính với r2 (%.2f) là %.2f",r2,result2);
+        System.out.printf("%n Bán kính với r3 (%.2f) là %.2f",r3,result3);
+        System.out.printf("%n Bán kính với r4 (%.2f) là %.2f",r4,result4);
+        System.out.printf("%n Bán kính với r5 (%.2f) là %.2f",r5,result5);
+
+    }
 }
