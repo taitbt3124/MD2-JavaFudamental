136109439339f97c27cd77a9ebc4324dffc732b7
taitbt3124 <taitbt3124@gmail.com>
2026-04-21 20:06:23 +0700

    Gioi2

---
+++ src/module_1/gioi2.java
@@ -0,0 +1,21 @@
+package module_1;
+
+import java.util.Scanner;
+
+public class gioi2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập chiều rộng: ");
+        float width = sc.nextFloat();
+
+        System.out.print("Nhập chiều dài: ");
+        float height = sc.nextFloat();
+
+        float area =  width * height;
+        float perimeter  = (width + height) *2;
+
+        System.out.printf("%n S= %.2f %n CV = %.2f", area, perimeter);
+    }
+}
